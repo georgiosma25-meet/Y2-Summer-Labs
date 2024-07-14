275d41cbb31a0052f590c35f387f8659424576df
georgiosma25-meet <gmattar2@js-bethlehem.com>
2024-07-14 21:36:01 +0300

    codon

---
+++ 2.1 python review #1/pythonreview1.py
@@ -0,0 +1,15 @@
+temp=[]
+
+
+
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(26,40))
+
+for i in range(7):
+	temp.append(random.randint(26,40))
+
+print(temp)
