6a25d98fc0d5bd23bb626c2c9f6eddb9fde2f789
georgiosma25-meet <gmattar2@js-bethlehem.com>
2024-07-18 10:51:47 +0300

    iihw'oia

---
+++ 0.3 FrontEnd Review/youtube.py
@@ -0,0 +1,31 @@
+def create_youtube_videos(title, description): 
+	myDict = {"title": title , "description": description, "likes": 0, "dislikes": 0, "comments": {}, "hashtag": []} 
+
+	return myDict 
+
+ 
+ 
+def like (myDict) : 
+	if "likes" in myDict : 
+		myDict["likes"]+=1  
+	return myDict 
+ 
+def dislike (myDict) : 
+	if "dislikes" in myDict : 
+		myDict["dislikes"]+=1 
+		return myDict 
+
+username = input ("Username: " ) 
+comment_text = input("Comment: ") 
+def add_comment (myDict, username, comment_text) : 
+	myDict["comments"][username]= comment_text 
+	return myDict  
+youtube_dict = create_youtube_videos("Sari's youtube video!", "My first video") 
+
+while youtube_dict["likes"] < 495:
+	youtube_dict["likes"] +=1 
+
+while len(youtube_dict["hashtag"]) < 5 : 
+	youtube_dict["hashtag"].append(input("Hashtag: "))
+
+print(youtube_dict)
